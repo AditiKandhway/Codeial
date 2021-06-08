1246492dc39cddf07d3a119aff66cd3087c5e682
Aditi Kandhway <aditikandhway1234@gmail.com>
2021-06-08 22:13:04 +0530

    Styling using scss

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const passport = require('passport');
 const passportLocal = require('./config/passport_local_strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware=require('node-sass-middleware');
-// app.use(sassMiddleware({
-//   src: '/assests/scss',
-//   dest:'/assests/css',
-//   debug:true,
-//   outputStyle:'extended',
-//   prefix:'/css'
-// }));
+app.use(sassMiddleware({
+  src: './assets/scss',
+  dest:'./assets/css',
+  debug:true,
+  outputStyle:'extended',
+  prefix:'/css'
+}));
 app.use(express.urlencoded());
 
 app.use(cookieParser());
