3e6008907798675330d2e3530a1e891c702b92cf
Aditi Kandhway <aditikandhway1234@gmail.com>
2021-07-01 22:00:36 +0530

    Delete and add post using AJAX

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,10 +3,20 @@ const Comment = require('../models/comment');
 module.exports.create = async function(req,res)
 {
   try{
-    await Post.create({
+    let post = await Post.create({
       content:req.body.content,
       user:req.user._id
     });
+    // check ajax request
+    if(req.xhr)
+    {
+      return res.status(200).json({
+        data:{
+          post:post
+        },
+        message:"Post Created!"
+      });
+    }
     req.flash('success', 'Post published!');
     return res.redirect('back');
   }catch(err){
@@ -25,6 +35,15 @@ module.exports.destroy = async function(req,res)
         post.remove();
         // deleteMany delets the comments based on some query.
         await Comment.deleteMany({post:req.params.id});
+        if(req.xhr)
+        {
+          return res.status(200).json({
+            data:{
+              post_id:req.params.id
+            },
+            message:"Post deleted"
+          });
+        }
         req.flash('success', 'Post and associated comments deleted!');
           return res.redirect('back');
       }
