fc01404b5a3efd38034808ab5c624abf85fff4f3
Aditi Kandhway <aditikandhway1234@gmail.com>
2021-07-05 00:28:59 +0530

    Enabled file upload

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -15,6 +15,17 @@
            let newPost = newPostDom(data.data.post);
            $('#post-container>ul').prepend(newPost);
            deletePost($(' .delete-post-button',newPost));
+           // call the create comment class
+             new PostComments(data.data.post._id);
+
+             new Noty({
+                 theme: 'relax',
+                 text: "Post published!",
+                 type: 'success',
+                 layout: 'topRight',
+                 timeout: 1500
+
+             }).show();
         },error:function(error)
         {
           console.log(error.responseText);
@@ -66,5 +77,17 @@ let deletePost = function(deleteLink){
 
       });
  }
+ // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+  let convertPostsToAjax = function(){
+      $('#posts-container>ul>li').each(function(){
+          let self = $(this);
+          let deleteButton = $(' .delete-post-button', self);
+          deletePost(deleteButton);
+          // get the post's id by splitting the id attribute
+          let postId = self.prop('id').split("-")[1]
+          new PostComments(postId);
+      });
+  }
   createPost();
+  convertPostsToAjax();
 }
