a53e4a9e75149ef8ca55436802fd71e579b35e23
Aditi Kandhway <aditikandhway1234@gmail.com>
2021-07-17 00:49:44 +0530

    Authorization using API

--- routes/api/v2/index.js
+++ routes/api/v2/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
 const router = express.Router();
-router.use('/posts',require('./posts'));
-router.use('/users',require('./users'));
+router.use('/comments',require('./comments'));
 module.exports = router;
